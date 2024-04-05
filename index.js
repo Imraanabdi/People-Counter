e760fce83e07ff44e01db46ced281bf0c914f82d
imranabdisalan7 <agmraan@gmail.com>
2024-04-05 12:06:55 +0300

    halfWay

---
+++ index.js
@@ -0,0 +1,57 @@
+// // let myAge=20
+// // console.log(myAge);
+
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count =firstBatch + secondBatch
+
+// // console.log(count);
+
+// let myAge = 1
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints);
+
+// function increment() {
+//     console.log("The button was clicked");
+// }
+
+// let lapsCompleted = 0
+
+
+// function incrementLaps() {
+//     lapsCompleted = lapsCompleted + 1
+// }
+
+// incrementLaps()
+// incrementLaps()
+// incrementLaps()
+// incrementLaps()
+
+
+
+// console.log(lapsCompleted);
+let countEl = document.getElementById("count-el")
+console.log(countEl);
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
+
+
+
+
